219fd7a21d144934bc5eb42e4741c41c445e6313
Dale Mathieu <dale.r.mathieu@hii-nns.com>
2018-04-16 21:02:40 -0400

    NameGame Finished
    NameGame Finished
    -Standard
    -Team Mode
    -Mat(t) Mode
    -Reverse Mode

---
+++ NameGame/Services/NameGameService.cs
@@ -0,0 +1,40 @@
+﻿using NameGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace NameGame.Services
+{
+    public class NameGameService
+    {
+        private const string DataUrl = "https://www.willowtreeapps.com/api/v1.0/profiles";
+
+        public async Task<Profile[]> GetProfiles()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(DataUrl);
+
+            //Get Response as JSON
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+            if(response.IsSuccessStatusCode)
+            {
+                // parsing
+                var dataProfiles = response.Content.ReadAsAsync<IEnumerable<Profile>>().Result;
+                Profile[] allProfiles = new Profile[dataProfiles.Count()];
+                int count = 0;
+                foreach(Profile p in dataProfiles)
+                {
+                    allProfiles[count] = p;
+                    count++;
+                }
+                return allProfiles;
+            }
+            return null;
+        }
+    }
+}
---
+++ NameGame/Services/StandardQuestion.cs
@@ -0,0 +1,56 @@
+﻿using NameGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//TODO: Explore Factory option further
+namespace NameGame.Services
+{
+    public class StandardQuestion
+    {
+
+        private NameGameService creator { get; set; }
+        private Profile[] ProfileList { get; set; }
+        public String[] profiles { get; set; }
+        public int correctIndex { get; set; }
+
+        public StandardQuestion()
+        {
+            creator = new NameGameService();
+            profiles = new String[5];
+            Random random = new Random();
+            correctIndex = random.Next(5);
+        }
+
+        //private async Task<Profile[]> GetProfiles()
+        //{
+        //    try
+        //    {
+        //        ProfileList = await creator.GetProfiles();
+        //    }
+        //    catch (Exception)
+        //    {
+        //        //TODO: Better Error Handling
+        //        return null;
+        //    }
+
+        //    Profile[] ProfilesToDisplay = new Profile[5];
+        //    Random randomIndex = new Random();
+        //    Profile toAdd;
+        //    int curr = 0;
+        //    while (curr != 5)
+        //    {
+        //        toAdd = ProfileList[randomIndex.Next(ProfileList.Length)];
+
+        //        if (!ProfilesToDisplay.Contains(toAdd) && toAdd.Headshot.Url != null)
+        //        {
+        //            //valid headshot and person isn't in list
+        //            //add to list
+        //            ProfilesToDisplay[curr] = toAdd;
+        //            curr++;
+        //        }
+        //    }
+        //}
+    }
+}
