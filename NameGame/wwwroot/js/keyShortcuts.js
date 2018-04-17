5225b8c4764c704b516c1d8a142a5cbb0d6af8a9
Dale Mathieu <dale.r.mathieu@hii-nns.com>
2018-04-17 19:59:00 -0400

    Moved keyShortcuts js function to own file and minified
    Moved keyShortcuts js function and minifed to reference on applicable pages.

---
+++ NameGame/wwwroot/js/keyShortcuts.js
@@ -0,0 +1,22 @@
+﻿$(window).keypress(function (key) {
+    switch (key.keyCode) {
+        case 49:
+            guess(0);
+            break;
+        case 50:
+            guess(1);
+            break;
+        case 51:
+            guess(2);
+            break;
+        case 52:
+            guess(3);
+            break;
+        case 53:
+            guess(4);
+            break;
+        default:
+            //do nothing
+            break;
+    }
+});
